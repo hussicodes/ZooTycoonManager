ea7fd4ce1e0e6e95b5dfcda8c37e6f72f6dca1ee
oliverslott <oliverslottsony@gmail.com>
2025-05-15 10:12:03 +0200

    Sqlite

--- GameWorld.cs
+++ GameWorld.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -18,6 +19,73 @@ namespace ZooTycoonManager
 
         protected override void Initialize()
         {
+            using var connection = new SqliteConnection("Data Source=mydb.db");
+            connection.Open();
+
+            var createTablesCmd = connection.CreateCommand();
+
+            createTablesCmd.CommandText = @"CREATE TABLE IF NOT EXISTS Habitat (
+                  habitat_id INTEGER PRIMARY KEY,
+                  size INTEGER NOT NULL,
+                  max_animals INTEGER NOT NULL,
+                  name NVARCHAR(50) NOT NULL,
+                  type NVARCHAR(50) NOT NULL
+                );
+                CREATE TABLE IF NOT EXISTS Shop (
+                  shop_id INTEGER PRIMARY KEY,
+                  type NVARCHAR(50) NOT NULL,
+                  cost INTEGER NOT NULL
+                );
+                CREATE TABLE IF NOT EXISTS Visitor (
+                    visitor_id INTEGER PRIMARY KEY,
+                    name NVARCHAR(50) NOT NULL,
+                    money INTEGER NOT NULL,
+                    mood INTEGER NOT NULL,
+                    hunger INTEGER NOT NULL,
+                    habitat_id INTEGER,
+                    shop_id INTEGER,
+                    FOREIGN KEY (habitat_id) REFERENCES Habitat(habitat_id),
+                    FOREIGN KEY (shop_id) REFERENCES Shop(shop_id)
+                );
+                CREATE TABLE IF NOT EXISTS Zookeeper (
+                    zookeeper_id INTEGER PRIMARY KEY,
+                    name NVARCHAR(50) NOT NULL,
+                    upkeep INTEGER NOT NULL,
+                    habitat_id INTEGER,
+                    FOREIGN KEY (habitat_id) REFERENCES Habitat(habitat_id)
+                );
+                CREATE TABLE IF NOT EXISTS [Transaction] (
+                    transaction_id INTEGER PRIMARY KEY,
+                    price INTEGER NOT NULL,
+                    datetime DATETIME NOT NULL,
+                    visitor_id INTEGER NOT NULL,
+                    shop_id INTEGER NOT NULL,
+                    FOREIGN KEY (visitor_id) REFERENCES Visitor(visitor_id) ON DELETE CASCADE,
+                    FOREIGN KEY (shop_id) REFERENCES Shop(shop_id) ON DELETE CASCADE
+                );
+                CREATE TABLE IF NOT EXISTS Species (
+                    species_id INTEGER PRIMARY KEY,
+                    name NVARCHAR(50) NOT NULL UNIQUE
+                );
+                CREATE TABLE IF NOT EXISTS Animal (
+                    animal_id INTEGER PRIMARY KEY,
+                    name NVARCHAR(50) NOT NULL,
+                    mood INTEGER NOT NULL,
+                    hunger INTEGER NOT NULL,
+                    stress INTEGER NOT NULL,
+                    habitat_id INTEGER NOT NULL,
+                    FOREIGN KEY (habitat_id) REFERENCES Habitat(habitat_id) ON DELETE RESTRICT
+                );
+                CREATE TABLE IF NOT EXISTS VisitorFavoriteSpecies (
+                    visitor_id INTEGER NOT NULL,
+                    species_id INTEGER NOT NULL,
+                    PRIMARY KEY (visitor_id, species_id),
+                    FOREIGN KEY (visitor_id) REFERENCES Visitor(visitor_id) ON DELETE CASCADE,
+                    FOREIGN KEY (species_id) REFERENCES Species(species_id) ON DELETE CASCADE
+                );";
+
+            createTablesCmd.ExecuteNonQuery();
+
             // TODO: Add your initialization logic here
 
             base.Initialize();
