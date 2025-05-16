9801f566ea3ac062eb55c5d9338118d00a3b190f
betaRei <malthereippuert42@gmail.com>
2025-05-16 09:56:40 +0200

    added mutex to prototype

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿using var game = new ZooTycoonManager.GameWorld();
-game.Run();
+﻿using System.Threading;
+using System;
+using ZooTycoonManager;
+
+namespace ZooTycoonManager
+{
+    public static class Program
+    {
+        private static Mutex mutex;
+
+        [STAThread]
+        static void Main()
+        {
+            bool createdNew;
+            mutex = new Mutex(true, "GameMutex", out createdNew);
+
+            if (!createdNew)
+            {
+                Console.WriteLine("Already running");
+                return;
+            }
+
+            using var game = new GameWorld();
+            game.Run();
+        }
+    }
+}
